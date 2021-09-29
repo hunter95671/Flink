e9024ca674c34d74f2b95a28b18abcabb625c71b
hunter95671 <1095672468@qq.com>
2021-09-29 09:13:28 +0800

    TableAPI

--- src/main/scala/com/hunter95/tabletest/KafkaPipelineTest.scala
+++ src/main/scala/com/hunter95/tabletest/KafkaPipelineTest.scala
@@ -1,7 +1,9 @@
 package com.hunter95.tabletest
 
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.DataTypes
 import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{Csv, Kafka, Schema}
 
 object KafkaPipelineTest {
   def main(args: Array[String]): Unit = {
@@ -10,5 +12,50 @@ object KafkaPipelineTest {
     env.setParallelism(1)
 
     val tableEnv = StreamTableEnvironment.create(env)
+
+    //2.从kafka读取数据
+    tableEnv.connect(new Kafka()
+      .version("0.11")
+      .topic("sensor")
+      .property("zookeeper.connect", "hadoop102:2181")
+      .property("bootstrap.servers", "hadoop102:9092")
+    )
+      .withFormat(new Csv())
+      .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+        .field("timestamp", DataTypes.BIGINT())
+        .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("kafkaInputTable")
+
+    //3.查询转换
+    //3.1简单转换
+    val sensorTable = tableEnv.from("kafkaInputTable")
+    val resultTable = sensorTable
+      .select('id, 'temperature)
+      .filter('id === "sensor_1")
+
+    //3.2聚合转换
+    val aggTable = sensorTable
+      .groupBy("id") //基于id分组
+      .select('id, 'id.count as 'count)
+
+    //4.输出到kafka
+    tableEnv.connect(new Kafka()
+      .version("0.11")
+      .topic("sinktest")
+      .property("zookeeper.connect", "hadoop102:2181")
+      .property("bootstrap.servers", "hadoop102:9092")
+    )
+      .withFormat(new Csv())
+      .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+        .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("kafkaOutputTable")
+
+    resultTable.insertInto("kafkaOutputTable")
+
+    env.execute("kafka pipeline test")
   }
 }
