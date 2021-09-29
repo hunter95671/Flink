00fe0e6098f6658afa70c1adc2a3968caf439c3b
hunter95671 <1095672468@qq.com>
2021-09-29 16:38:35 +0800

    time

--- src/main/scala/com/hunter95/tabletest/TimeAndWindowTest.scala
+++ src/main/scala/com/hunter95/tabletest/TimeAndWindowTest.scala
@@ -2,7 +2,9 @@ package com.hunter95.tabletest
 
 import com.hunter95.apitest.SensorReading
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.table.api.EnvironmentSettings
 import org.apache.flink.table.api.scala._
 import org.apache.flink.types.Row
@@ -11,7 +13,7 @@ object TimeAndWindowTest {
   def main(args: Array[String]): Unit = {
     val env=StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     val settings=EnvironmentSettings.newInstance()
       .useBlinkPlanner()
@@ -28,8 +30,12 @@ object TimeAndWindowTest {
         val arr = data.split(",")
         SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
       })
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
+        override def extractTimestamp(t: SensorReading): Long = t.timestamp*1000L
+      })
 
-    val sensorTable=tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp,'pt.proctime)
+    //val sensorTable=tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp,'pt.proctime)
+    val sensorTable=tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp,'timestamp.rowtime)
 
     sensorTable.printSchema()
     sensorTable.toAppendStream[Row].print()
